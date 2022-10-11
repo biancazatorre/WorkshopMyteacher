4f317ecfaacc778a709d70817eb7d3df68c933b9
Bianca Zatorre <112172388+biancazatorre@users.noreply.github.com>
2022-10-11 00:19:09 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+let teacherForms = document.getElementsByClassName ("teacher-contract")
+for (let teacherForm of teacherForms) {
+    teacherForm.addEventListener("submit", function (event) {
+        event.preventDefault() //evitar q a pg atualize
+        
+        let price = teacherForm.price.value
+        let hours = teacherForm.hours.value
+        let total = price * hours
+        
+        alert("O valor total do contrato é de R$" + total)
+
+
+    } )
+}
